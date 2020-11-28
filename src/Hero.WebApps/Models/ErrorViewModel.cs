f9185a12e5fb41c434e09818234cd917cc97b934
Dimmy Shu <dimmy.shu@hotmail.com>
2020-11-28 02:08:33 -0800

    initial project layout

---
+++ src/Hero.WebApps/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hero.WebApps.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
